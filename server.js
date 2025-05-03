51e15c85bd3934777aa988eadee5b227c3d87f67
akdo1979 <akdo70@yandex.ru>
2025-05-03 05:42:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -38,4 +38,5 @@ app.post("/gpt", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(Server running on port ${PORT}));
+console.log(`Server running on port ${PORT}`);
+
